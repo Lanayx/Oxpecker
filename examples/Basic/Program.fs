a317bb056441095dfc129a264c3327a82d8a10d2
Vladimir Shchur <odindafna2006@rambler.ru>
2024-01-05 10:20:33 +0300

    Refactoring

--- examples/Basic/Program.fs
+++ examples/Basic/Program.fs
@@ -1,45 +1,39 @@
 ﻿open System
-open System.Threading.Tasks
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 open Oxpecker
 open Oxpecker.Routing
 
-let handler1 : EndpointHandler =
-    fun (ctx : HttpContext) ->
-        ctx.WriteTextAsync "Hello World"
-
-let handler2 (firstName : string) (age : int) : EndpointHandler =
-    fun (ctx : HttpContext) ->
-        ctx.WriteTextAsync(sprintf "Hello %s, you are %i years old." firstName age)
-
-let handler3 (a : string) (b : string) (c : string) (d : int) : EndpointHandler =
-    fun (ctx : HttpContext) ->
-        ctx.WriteTextAsync(sprintf "Hello %s %s %s %i" a b c d)
-
-let setHttpHeaderMw key value : EndpointMiddleware =
-    fun (next : EndpointHandler) (ctx : HttpContext) ->
+let setHttpHeaderMw key value: EndpointMiddleware =
+    fun (next: EndpointHandler) (ctx: HttpContext) ->
         ctx.SetHttpHeader(key, value)
         task {
             do! next ctx
             Console.WriteLine($"Header {key} set")
         }
 
+let handler1: EndpointHandler =
+    fun ctx -> ctx.WriteTextAsync "Hello World"
+
+let handler2 (firstName: string) (age: int): EndpointHandler =
+    setHttpHeaderMw "foo" "var"
+    >=> _.WriteTextAsync(sprintf "Hello %s, you are %i years old." firstName age)
+
+let handler3 (a: string) (b: string) (c: string) (d: int): EndpointHandler =
+    _.WriteTextAsync(sprintf "Hello %s %s %s %i" a b c d)
 
 let endpoints =
     [
         subRoute "/foo" [
             GET [
-                route "/bar" (setHttpHeaderMw "too" "bar" >=> setHttpHeaderMw "foo" "bar" >=> text "Aloha!")
+                route "/bar" (setHttpHeaderMw "foo" "bar" >=> setHttpHeaderMw "moo" "gar" >=> text "FooBar!")
             ]
         ]
         GET [
             route  "/" (text "Hello World")
-            routef "/{%s}/{%i}" (fun name age ctx -> (setHttpHeaderMw "foo" "var" >=> handler2 name age) ctx)
+            routef "/{%s}/{%i}" handler2
             routef "/{%s}/{%s}/{%s}/{%i}" handler3
         ]
         GET_HEAD [
@@ -53,13 +47,13 @@ let endpoints =
         ]
     ]
 
-let configureApp (appBuilder : IApplicationBuilder) =
+let configureApp (appBuilder: IApplicationBuilder) =
     appBuilder
         .UseRouting()
         .UseOxpecker(endpoints)
     |> ignore
 
-let configureServices (services : IServiceCollection) =
+let configureServices (services: IServiceCollection) =
     services
         .AddRouting()
     |> ignore
--- src/Core.fs
+++ src/Core.fs
@@ -9,7 +9,7 @@ open Microsoft.AspNetCore.Http
 // Fsharp-friendly alias for RequestDelegate
 type EndpointHandler = HttpContext -> Task
 
-// Fsharp-friendly alias for RequestDelegate
+// Endpoint middleware is analogue to ASP.NET Core Middleware, but for Endpoint level.
 type EndpointMiddleware = EndpointHandler -> EndpointHandler
 
 let inline compose_opImpl (_ : ^OpImpl) xs1 xs2 =
@@ -46,8 +46,7 @@ type Composition =
                 | false -> resultMiddleware ctx
 
 /// <summary>
-/// Combines two <see cref="HttpHandler"/> functions into one.
-/// Please mind that both <see cref="HttpHandler"/> functions will get pre-evaluated at runtime by applying the next <see cref="HttpFunc"/> parameter of each handler.
+/// Combines two <see cref="EndpointHandler"/> or two <see cref="EndpointMiddleware"/> functions into one. Also can combine middleware with handler
 /// </summary>
 let inline (>=>) xs1 xs2 = compose_opImpl Unchecked.defaultof<Composition> xs1 xs2
 
@@ -56,7 +55,7 @@ let inline (>=>) xs1 xs2 = compose_opImpl Unchecked.defaultof<Composition> xs1 x
 /// Writes an UTF-8 encoded string to the body of the HTTP response and sets the HTTP Content-Length header accordingly, as well as the Content-Type header to text/plain.
 /// </summary>
 /// <param name="str">The string value to be send back to the client.</param>
-/// <returns>A Oxpecker <see cref="HttpHandler" /> function which can be composed into a bigger web application.</returns>
+/// <returns>A Oxpecker <see cref="EndpointHandler" /> function which can be composed into a bigger web application.</returns>
 let text (str : string) : EndpointHandler =
     let bytes = Encoding.UTF8.GetBytes str
     fun (ctx : HttpContext) ->
