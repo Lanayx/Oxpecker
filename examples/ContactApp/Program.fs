db56617e96aecb7e0207e8dd1af588175063b1c6
Vladimir Shchur <odindafna2006@rambler.ru>
2024-02-18 14:55:12 +0300

    ContactApp: HelloWorld

---
+++ examples/ContactApp/Program.fs
@@ -0,0 +1,62 @@
+﻿open System.Threading.Tasks
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+open Oxpecker
+open Oxpecker.ViewEngine
+
+let endpoints = [
+    GET [
+        route "/" <| text "Hello World"
+    ]
+]
+
+let errorView errorCode (errorText: string) =
+    html() {
+        body(style = "width: 800px; margin: 0 auto") {
+            h1(style = "text-align: center; color: red") { raw $"Error <i>%d{errorCode}</i>" }
+        }
+    }
+
+let notFoundHandler (ctx: HttpContext) =
+    let logger = ctx.GetLogger()
+    ctx.SetStatusCode 404
+    ctx.WriteHtmlView(errorView 404 "Page not found!")
+
+let errorHandler (ctx: HttpContext) (next: RequestDelegate) =
+    task {
+        try
+            return! next.Invoke(ctx)
+        with
+        | :? ModelBindException
+        | :? RouteParseException as ex ->
+            ctx.SetStatusCode StatusCodes.Status400BadRequest
+            return! ctx.WriteHtmlView(errorView 400 (string ex))
+        | ex ->
+            ctx.SetStatusCode StatusCodes.Status500InternalServerError
+            return! ctx.WriteHtmlView(errorView 500 (string ex))
+    }
+    :> Task
+
+let configureApp (appBuilder: IApplicationBuilder) =
+    appBuilder
+        .UseRouting()
+        .Use(errorHandler)
+        .UseOxpecker(endpoints)
+        .Run(notFoundHandler)
+
+let configureServices (services: IServiceCollection) =
+    services
+        .AddRouting()
+        .AddOxpecker()
+    |> ignore
+
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    configureServices builder.Services
+    let app = builder.Build()
+    configureApp app
+    app.Run()
+    0
