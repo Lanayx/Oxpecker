a3e3c1c3f07e4aba96063d7c340a79fccc610179
cabboose <shayanftw@gmail.com>
2025-02-18 12:01:28 +0800

    Including the test file would be smart.

---
+++ tests/Oxpecker.Solid.Tests/SolidCases/LibraryImportsChildren/LibraryImportsChildren.fs
@@ -0,0 +1,9 @@
+﻿module Oxpecker.Solid.Tests.Cases.LibraryImportsChildren
+
+open Oxpecker.Solid
+open Oxpecker.Solid.Tests.Cases.Types
+
+
+[<SolidComponent>]
+let Button () =
+    ImportedTag(class'="TEST").attr("key","value") { "Child" }
