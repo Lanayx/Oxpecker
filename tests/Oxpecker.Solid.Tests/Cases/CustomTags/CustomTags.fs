c9a211ede3245a3f6c62e553623acf47adbd3fe6
Vladimir Shchur <odindafna2006@rambler.ru>
2025-03-01 15:12:18 -0800

    Support for multiple extension methods for imported tag. #54

--- tests/Oxpecker.Solid.Tests/Cases/CustomTags/CustomTags.fs
+++ tests/Oxpecker.Solid.Tests/Cases/CustomTags/CustomTags.fs
@@ -3,9 +3,17 @@ module Oxpecker.Solid.Tests.Cases.CustomTags
 open System
 open Oxpecker.Solid
 open Oxpecker.Solid.Tests.Cases.MyTag
+open Oxpecker.Solid.Tests.Cases.ImportedTag
 
 [<SolidComponent>]
-let Test () =
+let Test2 () =
+    ImportedTag().attr("x", "y").attr("a", "b") {
+        ImportedTag().attr("x", "y").attr("a", "b")
+        "Hello"
+    }
+
+[<SolidComponent>]
+let Test1 () =
     MyTag(myAttr = "foo") {
         "Hello " + DateTime().Day.ToString()
         MyTag()
