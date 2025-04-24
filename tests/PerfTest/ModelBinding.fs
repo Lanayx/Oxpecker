a591e2163a738c29ef3f526471340985db91775c
Vladimir Shchur <odindafna2006@rambler.ru>
2025-04-24 11:30:56 -0700

    Simplified api model binding perf test

--- tests/PerfTest/ModelBinding.fs
+++ tests/PerfTest/ModelBinding.fs
@@ -107,28 +107,17 @@ type ModelBinding() =
 
     let requestBody =
         "Id=4e213598-5b45-4cd7-a87c-429d7b6b2f03&FirstName=Susan&MiddleName=Elisabeth&LastName=Doe&BirthDate=1986-12-29&StatusCode=200&Nicknames=Susi&Nicknames=Eli&Nicknames=Liz&Children[0].Name=Hamed&Children[0].Age=32&Children[1].Name=Ali&Children[1].Age=22&Children[2].Name=Gholi&Children[2].Age=44"
+    let content = new StringContent(requestBody, MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded"))
 
 
     [<Benchmark>]
     member this.OxpeckerPost() =
-        task {
-            use content =
-                new StringContent(requestBody, MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded"))
-            return! oxpeckerClient.PostAsync("/bindModel", content)
-        }
+        oxpeckerClient.PostAsync("/bindModel", content)
 
     [<Benchmark>]
     member this.GiraffePost() =
-        task {
-            use content =
-                new StringContent(requestBody, MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded"))
-            return! giraffeClient.PostAsync("/bindModel", content)
-        }
+        giraffeClient.PostAsync("/bindModel", content)
 
     [<Benchmark>]
     member this.MinimalApiPost() =
-        task {
-            use content =
-                new StringContent(requestBody, MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded"))
-            return! minimalApiClient.PostAsync("/bindModel", content)
-        }
+        minimalApiClient.PostAsync("/bindModel", content)
