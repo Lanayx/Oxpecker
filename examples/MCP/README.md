a97c5747fe04de80a116c9f26d58b56c9ca99462
Vladimir Shchur <odindafna2006@rambler.ru>
2025-05-20 16:29:04 -0700

    MCP try (SemanticKernel issue not resolved)

---
+++ examples/MCP/README.md
@@ -0,0 +1,3 @@
+## MCP example
+
+F# adaptation of https://www.linkedin.com/pulse/mcp-net-c-right-way-hosted-aspnetcore-aspire-what-else-latorre-zs3ff/
---
+++ examples/MCP/Server/Program.fs
@@ -0,0 +1,44 @@
+﻿open System
+open System.ComponentModel
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Logging
+open ModelContextProtocol.Server
+open Oxpecker
+
+[<Sealed>]
+[<McpServerToolType>]
+type UtcTimeTool() =
+
+    [<McpServerTool>]
+    [<Description("Gets the current UTC time.")>]
+    static member GetCurrentTime() : string =
+        DateTime.UtcNow.ToString()
+
+
+let endpoints = [
+    route "/" <| text "Hello World!"
+]
+
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    builder.Logging.AddConsole(fun options ->
+        options.LogToStandardErrorThreshold <- LogLevel.Trace
+    ) |> ignore
+    builder.Services
+        .AddRouting()
+        .AddOxpecker()
+        .AddMcpServer()
+            .WithTools<UtcTimeTool>()
+            .WithHttpTransport()
+        |> ignore
+    let app = builder.Build()
+    app
+        .UseRouting()
+        .UseOxpecker(endpoints)
+        |> ignore
+    app.MapMcp() |> ignore
+    app.Run()
+    0 // Exit code
