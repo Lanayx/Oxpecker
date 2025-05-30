3f3ed013b4350e7b17e3ce81af200c84174a8dab
Vladimir Shchur <odindafna2006@rambler.ru>
2025-05-30 16:53:40 -0700

    Maded svg test more real

--- tests/Oxpecker.Solid.Tests/Cases/Svg/Svg.fs
+++ tests/Oxpecker.Solid.Tests/Cases/Svg/Svg.fs
@@ -8,11 +8,11 @@ open Oxpecker.Solid.Svg
 let SvgTest () =
     svg() {
         defs() {
-            linearGradient(id = "gradient", x1 = "0%", y1 = "0%", x2 = "100%", y2 = "0%") {
-                stop(offset = "0%", style = "stop-color:rgb(255,255,0);stop-opacity:1")
-                stop(offset = "100%", style = "stop-color:rgb(255,0,0);stop-opacity:1")
+            linearGradient(id="gradient", x1="0%", y1 ="0%", x2="100%", y2="0%") {
+                stop(offset="0%", style="stop-color:rgb(255,255,0);stop-opacity:1")
+                stop(offset="100%", style ="stop-color:rgb(255,0,0);stop-opacity:1")
             }
         }
-        circle(id="circle", ``fill-opacity`` = "fillOpacity", ``clip-rule`` = "clipRule")
+        circle(id="circle", ``fill-opacity``="50%", ``clip-rule``="nonzero")
         "Sorry but this browser does not support inline SVG."
     }
