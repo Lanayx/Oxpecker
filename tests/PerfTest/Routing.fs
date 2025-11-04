5d45e7cb04e28649b39453d7e203538a9dd83e3e
Vladimir Shchur <odindafna2006@rambler.ru>
2025-11-04 13:52:30 -0800

    Updated Oxpecker to .NET 10

--- tests/PerfTest/Routing.fs
+++ tests/PerfTest/Routing.fs
@@ -86,18 +86,19 @@ module GiraffeRouting =
 [<MemoryDiagnoser>]
 type Routing() =
 
-    // BenchmarkDotNet v0.13.12, Windows 11 (10.0.22631.3296/23H2/2023Update/SunValley3)
-    // AMD Ryzen 5 5600H with Radeon Graphics, 1 CPU, 12 logical and 6 physical cores
-    // .NET SDK 8.0.200
-    //   [Host]     : .NET 8.0.3 (8.0.324.11423), X64 RyuJIT AVX2 DEBUG
-    //   DefaultJob : .NET 8.0.3 (8.0.324.11423), X64 RyuJIT AVX2
+    // BenchmarkDotNet v0.15.5, Windows 11 (10.0.26200.6899)
+    // AMD Ryzen 5 5600H with Radeon Graphics 3.30GHz, 1 CPU, 12 logical and 6 physical cores
+    // .NET SDK 10.0.100-rc.2.25502.107
+    //   [Host]     : .NET 10.0.0 (10.0.0-rc.2.25502.107, 10.0.25.50307), X64 RyuJIT x86-64-v3 DEBUG
+    //   DefaultJob : .NET 10.0.0 (10.0.0-rc.2.25502.107, 10.0.25.50307), X64 RyuJIT x86-64-v3
     //
-    // | Method            | Mean      | Error     | StdDev    | Gen0   | Allocated |
-    // |------------------ |----------:|----------:|----------:|-------:|----------:|
-    // | GetOxpeckerRoute  |  8.706 us | 0.1729 us | 0.3289 us | 0.9766 |   8.08 KB |
-    // | GetOxpeckerRoutef |  9.138 us | 0.1807 us | 0.2219 us | 0.9766 |    8.4 KB |
-    // | GetGiraffeRoute   |  9.611 us | 0.1918 us | 0.4741 us | 1.0986 |   9.48 KB |
-    // | GetGiraffeRoutef  | 26.128 us | 0.5105 us | 0.7948 us | 1.4648 |  13.11 KB |
+    //
+    // | Method            | Mean      | Error     | StdDev    | Median    | Gen0   | Allocated |
+    // |------------------ |----------:|----------:|----------:|----------:|-------:|----------:|
+    // | GetOxpeckerRoute  |  6.646 us | 0.1293 us | 0.3450 us |  6.575 us | 0.8545 |    7.8 KB |
+    // | GetOxpeckerRoutef |  7.357 us | 0.1451 us | 0.3029 us |  7.310 us | 0.9766 |   8.08 KB |
+    // | GetGiraffeRoute   |  7.596 us | 0.1467 us | 0.3542 us |  7.477 us | 1.0986 |   9.19 KB |
+    // | GetGiraffeRoutef  | 11.959 us | 0.2369 us | 0.5394 us | 11.927 us | 1.4648 |  12.63 KB |
 
     let oxpeckerServer = OxpeckerRouting.webApp()
     let giraffeServer = GiraffeRouting.webApp()
