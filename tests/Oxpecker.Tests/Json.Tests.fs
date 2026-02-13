e115e99a4b466dea2005d2861b49cd2a38b407b6
Vladimir Shchur <odindafna2006@rambler.ru>
2026-02-13 08:34:06 -0800

    Added more tests for serializer #91

--- tests/Oxpecker.Tests/Json.Tests.fs
+++ tests/Oxpecker.Tests/Json.Tests.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Text.Json
 open Microsoft.AspNetCore.Http
 open Microsoft.Net.Http.Headers
 open Oxpecker
@@ -47,6 +48,44 @@ let ``Test chunked serializer`` () =
     }
 
 [<Fact>]
+let ``Test not chunked serializer with custom options`` () =
+    task {
+        let options = JsonSerializerOptions(PropertyNamingPolicy = null) // Pascal case
+        let serializer: IJsonSerializer = SystemTextJsonSerializer(options)
+        let httpContext = DefaultHttpContext()
+        httpContext.Response.Body <- new MemoryStream()
+        let value = {| Name = "Oxpecker" |}
+        do! serializer.Serialize(value, httpContext, false)
+        let stream = httpContext.Response.Body
+        stream.Seek(0L, SeekOrigin.Begin) |> ignore
+        use streamReader = new StreamReader(stream)
+        let json = streamReader.ReadToEnd()
+        json |> shouldEqual """{"Name":"Oxpecker"}"""
+        httpContext.Response.Headers.ContentType
+        |> shouldEqual "application/json; charset=utf-8"
+        httpContext.Response.Headers.ContentLength |> shouldEqual 19L
+    }
+
+[<Fact>]
+let ``Test chunked serializer with custom options`` () =
+    task {
+        let options = JsonSerializerOptions(PropertyNamingPolicy = null) // Pascal case
+        let serializer: IJsonSerializer = SystemTextJsonSerializer(options)
+        let httpContext = DefaultHttpContext()
+        httpContext.Response.Body <- new MemoryStream()
+        let value = {| Name = "Oxpecker" |}
+        do! serializer.Serialize(value, httpContext, true)
+        let stream = httpContext.Response.Body
+        stream.Seek(0L, SeekOrigin.Begin) |> ignore
+        use streamReader = new StreamReader(stream)
+        let json = streamReader.ReadToEnd()
+        json |> shouldEqual """{"Name":"Oxpecker"}"""
+        httpContext.Response.Headers.ContentType
+        |> shouldEqual "application/json; charset=utf-8"
+        httpContext.Response.Headers.ContentLength |> shouldEqual(Nullable())
+    }
+
+[<Fact>]
 let ``Test default deserializer`` () =
     task {
         let serializer: IJsonSerializer = SystemTextJsonSerializer()
@@ -55,6 +94,22 @@ let ``Test default deserializer`` () =
         httpContext.Request.Headers[HeaderNames.ContentType] <- "application/json; charset=utf-8"
         use streamWriter = new StreamWriter(httpContext.Request.Body)
         streamWriter.Write("""{"name":"Oxpecker"}""")
+        streamWriter.Flush()
+        httpContext.Request.Body.Seek(0L, SeekOrigin.Begin) |> ignore
+        let! value = serializer.Deserialize<{| Name: string |}>(httpContext)
+        value |> shouldEqual {| Name = "Oxpecker" |}
+    }
+
+[<Fact>]
+let ``Test default deserializer with custom options`` () =
+    task {
+        let options = JsonSerializerOptions(PropertyNamingPolicy = null) // Pascal case
+        let serializer: IJsonSerializer = SystemTextJsonSerializer(options)
+        let httpContext = DefaultHttpContext()
+        httpContext.Request.Body <- new MemoryStream()
+        httpContext.Request.Headers[HeaderNames.ContentType] <- "application/json; charset=utf-8"
+        use streamWriter = new StreamWriter(httpContext.Request.Body)
+        streamWriter.Write("""{"Name":"Oxpecker"}""")
         streamWriter.Flush()
         httpContext.Request.Body.Seek(0L, SeekOrigin.Begin) |> ignore
         let! value = serializer.Deserialize<{| Name: string |}>(httpContext)
